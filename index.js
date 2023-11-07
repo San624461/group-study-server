ccdba0c27477216184fcf00b530524a70bbfd384
San624461 <sanjeda6244611@gmail.com>
2023-11-06 19:00:35 -0500

    delete operator implemented

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //middleware 
 app.use(cors());
 app.use(express.json());
 
-// sanjeda6244611
-// UKQ5rQahxMHvK02o
-console.log(process.env.DB_PASS);
 
 
 
@@ -28,14 +25,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+
+
+
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const AssignmentsCollection = client.db('assignmentsDb').collection('assignment')
+
+        // created assignmentd by users
+        app.post('/createdAssignments', async (req, res) => {
+            const newAssignment = req.body
+            console.log(newAssignment);
+            const result = await AssignmentsCollection.insertOne(newAssignment)
+            res.send(result)
+        })
+
+        // getting all assignments
+        app.get('/createdAssignments', async (req, res) => {
+            const cursor = await AssignmentsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        // delete one assignment based on one's email from all assignment
+        app.delete('/createdAssignments/:id', async (req, res) => {
+            const id = req.params.id
+
+            const query = { _id: new ObjectId(id) }
+            const result = await AssignmentsCollection.deleteOne(query)
+            res.send(result)
+        })
+        // update the specific assignment one user created
+
+        app.get('/createdAssignments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await AssignmentsCollection.findOne(query)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
